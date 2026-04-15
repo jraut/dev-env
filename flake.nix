4c556fe7dc2fae9e334749449a70c9524c374430
Juho Rautioaho <juhorautioaho@gmail.com>
2026-04-15 20:41:49 +0300

    style: formatting

--- flake.nix
+++ flake.nix
@@ -3,77 +3,79 @@
     nixpkgs.url = "github:NixOS/nixpkgs";
   };
 
-
-  outputs = { self, nixpkgs }: {
+  outputs = {
+    self,
+    nixpkgs,
+  }: {
     packages."aarch64-darwin".default = let
       pkgs = nixpkgs.legacyPackages."aarch64-darwin";
-    in pkgs.buildEnv {
-      name = "home-packages";
-      paths = with pkgs; [
+    in
+      pkgs.buildEnv {
+        name = "home-packages";
+        paths = with pkgs; [
+          # ... add your tools here
 
-		# ... add your tools here
- 
-    # tooling
-   fd
-   jq
-   bat-extras.prettybat
-   bat
-   tealdeer
-    curlie
-   httpie
-   delta
-   difftastic
-   pgcli
-   postgresql
-   pgadmin4
-   just
-   lazygit
-   ripgrep
-   just
+          # tooling
+          fd
+          jq
+          bat-extras.prettybat
+          bat
+          tealdeer
+          curlie
+          httpie
+          delta
+          difftastic
+          pgcli
+          postgresql
+          pgadmin4
+          just
+          lazygit
+          ripgrep
+          just
 
-    # sublime-merge unfree
-   sd
-    #dust
+          # sublime-merge unfree
+          sd
+          #dust
 
-    # zsh, shell environment, osx utils etc
-   starship
-   tmux
-   jump
-   rectangle # resize windows
-   iterm2
+          # zsh, shell environment, osx utils etc
+          starship
+          tmux
+          jump
+          rectangle # resize windows
+          iterm2
 
-   # browsers, web
-   brave
-   firefox
-  #  ungoogled-chromium 
+          # browsers, web
+          brave
+          firefox
+          #  ungoogled-chromium
 
-   # arc-browser unfree
-   # spotify unfree
-   # slack unfree
-   slackdump
-  
-  # editors tools code
-  zed-editor
-  vscodium
+          # arc-browser unfree
+          # spotify unfree
+          # slack unfree
+          slackdump
 
-    # programming envs, compilers and runtimes
-   nim
-   uv 
-   docker
-   git
-   jujutsu
-   cmake
-  alejandra # nix source code formatter  
+          # editors tools code
+          zed-editor
+          vscodium
 
-   # nodejs manage outside of nix to use nvm - see https://www.uglydirtylittlestrawberry.co.uk/posts/using-nvm-with-yarn-nix-shells/
-   # yarn manage outside of nix to use nvm
-    # (yarn.override { nodejs = null; })
+          # programming envs, compilers and runtimes
+          nim
+          uv
+          docker
+          git
+          jujutsu
+          cmake
+          alejandra # nix source code formatter
 
-   # ai
-   ollama
-   claude-code
-   github:akitaonrails/ai-jail
-       ];
-    };
+          # nodejs manage outside of nix to use nvm - see https://www.uglydirtylittlestrawberry.co.uk/posts/using-nvm-with-yarn-nix-shells/
+          # yarn manage outside of nix to use nvm
+          # (yarn.override { nodejs = null; })
+
+          # ai
+          ollama
+          claude-code
+          github:akitaonrails/ai-jail
+        ];
+      };
   };
 }
